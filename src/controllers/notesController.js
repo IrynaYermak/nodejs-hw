10d1ddebfc183cebdb649b6d94f5255418366a45
Iryna Yermak <1991irenka@gmail.com>
2026-01-29 17:19:42 +0000

    made correctioms

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -5,7 +5,7 @@ export const getAllNotes = async (req, res) => {
   const { page = 1, perPage = 10, tag, search } = req.query;
   const skip = (page - 1) * perPage;
 
-  const notesQuery = Note.findOne({ userId: req.userId });
+  const notesQuery = Note.find({ userId: req.user._id });
 
   if (tag) {
     notesQuery.where('tag').equals(tag);
